d3e9d7761dc8237ea5834885a78cd347b02790dc
kunjinjaala <mhlmi9236@yahoo.com>
2023-02-16 20:50:14 -0800

    First C programming

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Main - entry point
+ * Return always 1 (success)
+ **/
+int main(void)
+{
+	("and that piece of art is useful\" - Dora Korpar 2015-10-19,\n");
+	return (1);
+}
