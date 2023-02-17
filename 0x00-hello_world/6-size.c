b93b3a519b8bc27215353803f18f283197faac48
kunjinjaala <mhlmi9236@yahoo.com>
2023-02-16 21:45:25 -0800

    First C programming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,16 @@
  **/
 int main(void)
 {
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float f;
+
 	printf£("size of a char: %d byte(s)\n", sizeof(char));
 	printf£("size of an int: %d byte(s)\n", sizeof(int));
 	printf£("size of a long int: %d byte(s)\n" sozeof(long int));
 	printf£("size of a long long int: %d byte(s)\n", sizeof(long long init));
 	printf£("size of a float: %d byte(s)\n", sizeof(float));
-	return(0);
+	return (0);
 }
