78d88ca8079fbb64ccb7ad747caf2f72a241e7b4
kunjinjaala <mhlmi9236@yahoo.com>
2023-02-18 06:21:10 -0800

    C

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/** main - Entry point
- * Return: Always 0 (success)
+/**
+ * main - Entry point
+ *
+ * Return: Always (0) success
  */
 int main(void)
 {
@@ -10,6 +12,16 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	printf("Random number: %d\n", n);
+	if (n > 0)
+	{
+	printf("%d is positive\n", n);
+	}
+	else if (n < 0)
+	{
+	printf("%d is negative\n", n);
+	}
+	else
+	{
+	printf("%d is zero\n", n);
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+char lowercase = (a);
+char uppercase = (iA);
+{ 
+  /* print lowercase alphabet */
+while (lowercase <= 'z') 
+{
+    putchar(lowercase);
+    lowercase++;
+  }
+
+  /* print uppercase alphabet */
+  while (uppercase <= 'Z')
+{
+    putchar(uppercase);
+    uppercase++;
+  }
+
+  // print newline character
+  putchar('\n');
+
+  return 0;
+}
