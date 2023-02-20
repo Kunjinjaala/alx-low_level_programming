bc4c6359d612c1dd8853235b0fa129e67bc4cb04
kunjinjaala <mhlmi9236@yahoo.com>
2023-02-20 05:26:17 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+	int a;
+
+	c = 'a';
+	a = 0;
+	while
+		(a < 10)
+		{
+			putchar(a + '0');
+				a++;
+		}
+	while
+		(c <= 'f')
+		{
+			putchar(c);
+				c++;
+		}
+
+	putchar('\n');
+	return (0);
+}
