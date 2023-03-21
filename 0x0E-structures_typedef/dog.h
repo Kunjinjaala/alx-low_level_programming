a7855b4aeb939175de926d8815b06e068e0fe5f8
kunjinjaala <mhlmi9236@yahoo.com>
2023-03-20 23:17:52 -0700

    alx-low_level_programming/0x0E-structures_typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,8 +3,11 @@
 
 /**
  * struct dog - a dog's basic info
+ *
  * @name: First member
+ *
  * @age: Second member
+ *
  * @owner: Third member
  *
  * Description: Longer description
@@ -18,11 +21,10 @@ struct dog
 };
 
 /**
- * dog_t - typedef for struct dog
+ * dog_t - New type struct dog def
  */
 
 typedef struct dog dog_t;
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
