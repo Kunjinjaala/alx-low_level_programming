35a4df46945a4a8be5096d7ab9faf5b0592c1c44
kunjinjaala <mhlmi9236@yahoo.com>
2023-04-12 17:09:40 -0700

    alx-low_level_programming/0x15-file_io

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * read_textfile- Read text file print to STDOUT.
@@ -13,3 +14,31 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	char *buf;
+	ssize_t t_read;
+	ssize_t t_w;
+	FILE *fp;
+
+
+	if (filename == NULL)
+		return (0);
+	fp = fopen(filename, "r");
+
+	if (fp == NULL)
+		return (0);
+
+	buf = malloc(letters + 1);
+
+	if (buf == NULL)
+	fclose(fp);
+	return (0);
+
+	t_read = fread(buf, 1, letters, fp);
+	buf[t_read] = '\0';
+
+	t_w = fwrite(buf, 1, t_read, stdout);
+	fclose(fp);
+	free(buf);
+
+	return (t_w);
+}
