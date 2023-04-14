da4dad4573febb60671f2e829d4dbfedf2a9bc03
kunjinjaala <mhlmi9236@yahoo.com>
2023-04-13 20:09:30 -0700

    alx-low_level_programming/0x15-file_io

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -26,7 +26,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (fd == -1)
 		return (0);
 
-	buf = malloc(sizeof(char)*letters);
+	buf = malloc(sizeof(char) * letters);
 
 	t_read = read(fd, buf, letters);
 	buf[t_read] = '\0';
--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,23 +12,22 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, w;
+	int fd, t_w, len;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
-	w = write(fd, text_content, len);
-
-	if (fd == -1 || w == -1)
-		return (-1);
-
-	int len = 0;
+	len = 0;
 	if (text_content != NULL)
 	{
 		while (text_content[len] != '\0')
 			len++;
 	}
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
+	t_w = write(fd, text_content, len);
+
+	if (fd == -1 || t_w == -1)
+		return (-1);
 
 	close(fd);
 
