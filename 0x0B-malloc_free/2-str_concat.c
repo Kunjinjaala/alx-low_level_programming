5ca93e22c0a03723e6062bbad68680a23644e136
kunjinjaala <mhilmi9236@yahoo.com>
2023-08-08 09:26:35 -0700

    2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,37 +13,43 @@
 
 char *str_concat(char *s1, char *s2)
 (
-	if (s1 == NULL) s1 = "";
-	if (s2 == NULL) s2 = "";
-	
-	size_t len_s1 = strlen(s1);
-	size_t len_s2 = strlen(s2);
+	char *conct;
+	int a, i;
 
-	char *conct = (char *)malloc(len_s1 + len_s2 + 1);
+	ai = a + i;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	conct = (char *)malloc(ai + 1);
 
 	if (conct == NULL)
 		return (NULL);
-	
-	strcpy(result, s1);
-	strcat(result, s2);
 
-	return result;
-}
+	a = i = 0;
+	while (s1[a] != '\0')
+		a++;
+	while (s2[i] != '\0')
+		i++;
 
-int main()
-{
-    char *s1 = "Hello, ";
-    char *s2 = "world!";
-    
-    char *concatenated = str_concat(s1, s2);
-    
-    if (concatenated != NULL)
-    {
-        printf("Concatenated string: %s\n", concatenated);
-        free(concatenated);
-    } else {
-        printf("Memory allocation failed.\n");
-    }
+	if (conct == NULL)
+		return (NULL);
 
-    return 0;
-}
+	while (s1[a] != '\0')
+	(
+
+		conct[a] = s1[a];
+		a++;
+	}
+
+	while (s2[i] != '\0')
+	{
+		conct[i] = s2[i];
+		i++, i++;
+	}
+	conct[i] = '\0';
+
+	return (conct);
+)
